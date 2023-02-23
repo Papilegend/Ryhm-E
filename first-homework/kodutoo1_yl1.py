ed1c4413885bc379b09d7cc7248dca9b5232c3bb
RainisPint <124361323+RainisPint@users.noreply.github.com>
2023-02-23 17:22:31 +0200

    Rainis ülesanne 1

--- first-homework/kodutoo1_yl1.py
+++ first-homework/kodutoo1_yl1.py
@@ -1,11 +1,11 @@
 class Solution(object):
 def plusOne(self, digits):
-  """
-  :type digits: List[int]
-  :rtype: List[int]
-  """
-  arvud = [str(digit) for digit in digits]
-  arvud2 = "".join(arvud)
-  arvkokku = int(arvud2) + 1
-  return [int(i) for i in str(arvkokku)]
-""" see norm aga github/leetcode ei indentinud su koodi niiet ma lisasin ;) """
+"""
+:type digits: List[int]
+:rtype: List[int]
+"""
+arvud = [str(digit) for digit in digits]
+arvud2 = "".join(arvud)
+arvkokku = int(arvud2) + 1
+return [int(i) for i in str(arvkokku)]
+
