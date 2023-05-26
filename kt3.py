70fe01ab4a14d66f661deafef1e86a974964cb55
Tristan <119224484+Papilegend@users.noreply.github.com>
2023-05-26 19:18:42 +0300

    Update kt3.py
    Parandasin ära vead. Lisasin juurde selle, et kui on kasutajanimi juba andmebaasis olemas, siis ei saa seda kasutada ning tuleb uus kasutajanimi valida. 
    
    Eraldi sqli faili pole vaja, sest andmebaas asub täielikult https://www.freemysqlhosting.net/ platvormil.

--- kt3.py
+++ kt3.py
@@ -1,6 +1,7 @@
-import mysql.connector
-import bcrypt
+import mysql.connector # Impordin MySQLi Connector mooduli
+import bcrypt # Impordin bcrypti mooduli soolamiseks ja hashimiseks
 
+# Tekitan ühenduse https://www.freemysqlhosting.net andmebaasiga
 conn = mysql.connector.connect(
     host='sql7.freemysqlhosting.net',
     port=3306,
@@ -8,44 +9,66 @@ conn = mysql.connector.connect(
     user='sql7620863',
     password='9T7P1uHFr6'
 )
-cursor = conn.cursor()
+cursor = conn.cursor()  # Tekitan kursoriobjekti SQL-päringute täitmiseks
 
 
 def hash_password(password):
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode(), salt)
+    # Genereerin suvalise soolamise väärtuse
+    sool = bcrypt.gensalt()
+
+    # Hashin salasõna kasutades bcrypti
+    hashed_password = bcrypt.hashpw(password.encode(), sool)
     return hashed_password
 
 def registeerima():
-    kasutajanimi = input("Sisesta kasutajanimi: ")
+    # Loon while tsükli, mis kestab nii kaua, kui on valitud kasutajanimi, mis pole andmebaasis
+    kasutaja_on = True
+    while kasutaja_on: 
+
+        # Küsin kasutajalt kasutajanime ja salasõna
+        kasutajanimi = input("Sisesta kasutajanimi: ")
+
+        # Vaatan, kas kasutajanimi on võetud või mitte
+        paring = "SELECT kasutajanimi FROM kasutajad WHERE kasutajanimi = %s"
+        vaartused = (kasutajanimi,)
+        cursor.execute(paring, vaartused)
+        result = cursor.fetchone()
+
+        if result:
+            print("Kasutajanimi on juba võetud!")
+        else:
+            kasutaja_on = False
+
     password = input("Sisesta salasõna: ")
 
-    # Hashi ja soola salasõna
+    # Hashin ja soolan alaprogrammi abil salasõna
     hashed_password = hash_password(password)
 
-    # Salvesta kasutaja andmebaasi
-    query = "INSERT INTO kasutajad (kasutajanimi, password) VALUES (%s, %s)"
-    values = (kasutajanimi, hashed_password)
-    cursor.execute(query, values)
+    # Salvestan kasutaja andmebaasi
+    paring = "INSERT INTO kasutajad (kasutajanimi, password) VALUES (%s, %s)"
+    vaartused = (kasutajanimi, hashed_password)
+    cursor.execute(paring, vaartused)
     conn.commit()
 
     print("Kasutaja on edukalt registeeritud!")
 
 
 def login():
-    username = input("Sisesta kasutajanimi: ")
+
+    # Küsin kasutajalt kasutajanime ja salasõna
+    kasutajanimi = input("Sisesta kasutajanimi: ")
     password = input("Sisesta salasõna: ")
 
-    # Retrieve the stored hashed password for the entered username
-    query = "SELECT password FROM kasutajad WHERE kasutajanimi = %s"
-    values = (username,)
-    cursor.execute(query, values)
+    # Hangin andmebaasist sisestatud kasutajanime jaoks salvestatud hashitud ja soolatud salasõna
+    paring = "SELECT password FROM kasutajad WHERE kasutajanimi = %s"
+    vaartused = (kasutajanimi,)
+    cursor.execute(paring, vaartused)
     result = cursor.fetchone()
 
     if result:
         stored_password = result[0]
 
-        # Check if the entered password matches the stored hashed password
+        # Vaatan, kas sisestatud salasõna sobitud andmebaasis oleva hashitud salasõnaga 
         if bcrypt.checkpw(password.encode(), stored_password.encode()):
             print("Edukalt sisse loginud!")
         else:
@@ -53,7 +76,11 @@ def login():
     else:
         print("Ei leia kasutajat")
 
-registeerima()
-login()
+# Küsin kasutajalt, millist funktsiooni ta kasutada tahab
+valik = int(input("Sisselogimine - vajuta 0\nRegisteerimine - vajuta 1\nValik: "))
 
+if valik == 0:
+    login() # Kutsun välja sisselogimise 
+elif valik == 1:
+    registeerima() # Kutsun välja kasutaja registeerimise 
 conn.close()
